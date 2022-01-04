8721d85ad2ae7859b23fd1a91abc6f6ab231a80c
nvsnv303 <97093922+nvsnv303@users.noreply.github.com>
2022-01-04 17:23:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "iu tlc"
+    introTitle: 'Trần Linh Chi',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    title: 'Sau học kì I thì tớ nghĩ là tớ thật sự thích cậu',
+    desc: 'Cậu muốn làm ngừi iu tớ hong <3 ',
+    btnYes: 'Cóa <33',
+    btnNo: 'Mơ à :3',
+    question: 'Sao lại đồng ý zạ <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
